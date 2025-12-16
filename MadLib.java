8dd89fa9b2cc4e4cae57f3148071386c089f1912
Diego4322 <gldenpvp1@gmail.com>
2025-12-16 21:14:40 +0000

    beginning

---
+++ MadLib.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class MadLib {
+    public static void main(String[] args){
+        System.out.println("WELCOME TO MAD LIBS");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a name(noun): ");
+        String name = input.nextLine();
+        System.out.print("Please enter a verb: ");
+        String verb1 = input.nextLine();
+        System.out.print("Please enter a persons name (noun): ");
+        String name2 = input.nextLine();
+        System.out.print("Please enter an adjective: ");
+        String adj = input.nextLine();
+        System.out.print("Please enter an adjective: ");
+        String adj2 = input.nextLine();
+        System.out.print("Please enter a verb: ");
+        String verb = input.nextLine();
+        System.out.print("Please enter a noun: ");
+        String noun = input.nextLine();
+        System.out.print("Please enter a noun: ");
+        String noun2 = input.nextLine();
+        System.out.print("Please enter a noun: ");
+        String noun3 = input.nextLine();
+        System.out.print("Please enter an adjective: ");
+        String adj3 = input.nextLine();
+
+        
+
+        
+
+        System.out.println("Once I, " + name + ", genuinely");
+
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Mad-lib
+PARTNER: SOURISH DAS
